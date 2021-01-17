3bce6337b5d0933a5125ca8ec872f8e11a323b81
Roger Dahl <git@dahlsys.com>
2021-01-16 22:12:18 -0700

    Initial work

--- init.sh
+++ init.sh
@@ -8,25 +8,20 @@
 [[ "$0" == "$BASH_SOURCE" ]] && ret='exit' || ret='return'
 
 # Skip configuration if not running interactively.
-case $- in
-*i*) ;;
+[[ $- == *i* ]] || $ret
 
-*)
-  echo >&2 'bashrc.d skipped: Shell is not interactive'
-  $ret 0
-  ;;
-esac
+# Display 'last' info for login shells
+shopt -q login_shell && {
+  perl -pe 's/(\d\d)T(\d\d)/$1 $2/g; s/:00 / /g; s/wtmp.*\n//g;' \
+    <(last --hostlast --dns --time-format iso --present now --fullnames -3 | tac)
+}
 
-test -n "$BASHRC_DIR" || {
+[[ -n "$BASHRC_DIR" ]] || {
   BASHRC_DIR="$(dirname "$0")"
 }
 
-#echo >&2 "BASHRC_DIR: $BASHRC_DIR"
-
-#$ret 0
-
 for sh in "$BASHRC_DIR"/*.sh; do
-  test "$(basename "$sh")" != "init.sh" && {
+  [[ "$(basename "$sh")" != "init.sh" ]] && {
     printf "\rbashrc.d - $sh"
     tput ed
     # shellcheck disable=SC1090
@@ -37,5 +32,3 @@ done
 printf "\r"
 tput ed
 
-perl -pe 's/(\d\d)T(\d\d)/$1 $2/g; s/:00 / /g; s/wtmp.*\n//g;' \
-  <(last --hostlast --dns --time-format iso --present now --fullnames -3 | tac)
