876e52e323d430f214c6f02ad5d5f60b60048f17
Roger Dahl <git@dahlsys.com>
2021-02-01 11:02:07 -0700

    Initial work

--- 30-git-bashrc.sh
+++ 30-git-bashrc.sh
@@ -1,9 +1,6 @@
 # Shortcuts for dealing with .bashrc itself.
 
-#shopt -s extglob
-
-
-bashrc_cd() {
+rc_cd() {
   cd "$BASHRC_DIR" || {
     echo "Error: It doesn't look like we're in a bashrc.d environment (missing \$BASHRC_DIR)"
     return 1
@@ -13,11 +10,10 @@ bashrc_cd() {
 # git add all numbered bashrc.d scripts and push
 # Using a subshell, so the interactive CWD does not change.
 # This somehow breaks implicit exports with "set -a" ?
-bashrc_push() {
-#  shopt -s extglob
-
-  bashrc_cd || return 1
+rc_push() {
+  rc_cd || return 1
   # The extglob (regex glob) must not be quoted.
+  # This will return an error if `shopt -s extglob` has not been set.
   git add +([0-9][0-9]-*.sh)
   git commit -a -m 'Initial work'
   git push
@@ -25,7 +21,13 @@ bashrc_push() {
 
 # git pull bashrc.d
 # Using a subshell, so the interactive CWD does not change.
-bashrc_pull() (
-  bashrc_cd || return 1
+rc_pull() (
+  rc_cd || return 1
   git pull
 )
+
+# Launch a new shell, replacing current.
+rc() {
+  exec bash
+}
+
