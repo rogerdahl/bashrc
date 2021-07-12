4e4b9046138aa80afca0f90e7e872c36203c0fb3
rogerdahl <git@dahlsys.com>
2021-07-12 07:26:34 -0600

    Initial work

--- 30-fzf.sh
+++ 30-fzf.sh
@@ -1,2 +1,6 @@
+# fzf
+
+# Autocompletion and keybindings for bash
+# Usage examples: https://github.com/junegunn/fzf#examples
 [[ $- == *i* ]] && source "$BASHRC_DIR/bin/fzf-completion.bash" 2> /dev/null
 source "$BASHRC_DIR/bin/fzf-key-bindings.bash"
