d9a8a456de12c9980c29d40f078d1dac43984d2a
rogerdahl <git@dahlsys.com>
2026-04-22 13:46:38 -0600

    initial work

---
+++ 30-stl.sh
@@ -0,0 +1,18 @@
+mk_stl_thumbnails() {
+  if [ ! -d thumb ] ; then
+    mkdir thumb
+  fi
+
+  for e in stl STL ; do
+    for i in *.$e  ; do
+      j=$(basename "$i" .$e)
+      if [ ! -f thumb/"$j".png -o "$i" -nt thumb/"$j".png ] ; then
+        echo $j
+        echo "import(\"$i\");" > x.scad
+        openscad -o thumb/"$j".png --imgsize=100,100 x.scad
+      fi
+    done
+  done
+
+  rm -f x.scad
+}
